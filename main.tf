d283924720f14dbcb20a26a49a1c5bb90a1fcc8c
Jiawei Luo <jiawei.luo@q4inc.com>
2022-08-22 14:09:28 -0400

    Fix PR

--- main.tf
+++ main.tf
@@ -9,8 +9,4 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = var.aws_region
-}
-
 data "aws_caller_identity" "current_user" {}
--- variables.tf
+++ variables.tf
@@ -1,12 +1,48 @@
-variable "aws_region"             {}
-variable "date_format"            {}
-variable "provisioner"            {}
-variable "contact"                {}
-variable "environment"            {}
-variable "control_repository_url" {}
-variable "module_repository_url"  {}
-variable "name"                   {}
-variable "role"                   {}
-variable "product"                {}
-variable "description"            {}
-variable "additional_tags"        {}
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+variable "date_format" {
+  type    = string
+  default = "DD MMM YYYY hh:mm ZZZ"
+}
+variable "provisioner" {
+  type    = string
+  default = ""
+}
+variable "contact" {
+  type    = string
+  default = ""
+}
+variable "environment" {
+  type    = string
+  default = ""
+}
+variable "control_repository_url" {
+  type    = string
+  default = ""
+}
+variable "module_repository_url" {
+  type    = string
+  default = "git@github.com:q4mobile/terraform-module-aws-tags-default.git"
+}
+variable "name" {
+  type    = string
+  default = "terraform-module-aws-tags-default"
+}
+variable "role" {
+  type    = string
+  default = ""
+}
+variable "product" {
+  type    = string
+  default = ""
+}
+variable "description" {
+  type    = string
+  default = ""
+}
+variable "additional_tags" {
+  type    = map
+  default = null
+}
