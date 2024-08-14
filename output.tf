1f77ebc57b988b6a8c549e824ff64cdcb1cf13c6
Chris Jones <chrisj@q4inc.com>
2024-08-14 14:36:13 -0400

    fix: put ops_tier back and retagged with 2.1.0

--- output.tf
+++ output.tf
@@ -10,7 +10,7 @@ output "tags" {
     Name                = var.name
     Product             = var.product
     SubProduct          = var.sub_product
-    ops_tier            = var.product
+    ops_tier            = var.ops_tier
     ops-product         = var.sub_product
     Description         = var.description
     },
